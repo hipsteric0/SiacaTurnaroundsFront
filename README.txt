54743012977a6d731d26efcd7669eef87d7c97fa
hipsteric0 <aleromerosurf@gmail.com>
2023-06-12 12:22:26 -0400

    trying to make api calls work

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 npm install  --- instala dependencias nuevas
-npm run dev --- correr el proyecto. la direccion para correr el proyecto debe verse asi C:\Users\alero\Desktop\SiacaTurnarounds\SiacaTurnaroundsFront\siaca-turnarounds-front
+npm run dev --- correr el proyecto. la direccion para correr el proyecto debe verse asi 
+C:\Users\alero\Desktop\SiacaTurnarounds\SiacaTurnaroundsFront\SiacaTurnaroundsFront\siaca-turnarounds-front
--- siaca-turnarounds-front/src/pages/index.tsx
+++ siaca-turnarounds-front/src/pages/index.tsx
@@ -2,14 +2,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import handler from './api/api-endpoints'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+const [state,setState]= useState();
+
+const address = fetch("http://127.0.0.1:8000/api/maquinaria")
+  .then((response) => response.json())
+  .then((user) => {
+    return user.address;
+  });
+
+const printAddress = async () => {
+  const a = await address;
+  console.log(a);
+};
+
+
+printAddress();
   return (
     <>
+
       <main>
         pantalla de login
+        <button >
+          llamada API
+        </button>
       </main>
     </>
   )
