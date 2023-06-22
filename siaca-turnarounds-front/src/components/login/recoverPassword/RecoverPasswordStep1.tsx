a35c398c047d485815236c93f570ce8ef95991de
hipsteric0 <aleromerosurf@gmail.com>
2023-06-22 17:51:36 -0400

    step1 mobile y desktop recuperrar contrasena

--- siaca-turnarounds-front/src/components/login/recoverPassword/RecoverPasswordStep1.tsx
+++ siaca-turnarounds-front/src/components/login/recoverPassword/RecoverPasswordStep1.tsx
@@ -1,6 +1,9 @@
-import styles from "./recoverPasswordStep1.style.module.css";
+import styles from "./RecoverPasswordStep1.style.module.css";
 import Image from "next/image";
 import { useMediaQuery } from "@mui/material";
+import PasswordIcon from "@mui/icons-material/Password";
+import { ClassNames } from "@emotion/react";
+import BackArrow from "@/components/Reusables/BackArrow";
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -8,7 +11,49 @@ interface PageProps {
 
 const LoginMainPage: React.FC<PageProps> = ({ setStep }) => {
   const isMobile = useMediaQuery("(max-width: 1270px)");
-  return <div className={styles.clase1}>recoverpassword step 1</div>;
+  return (
+    <div
+      className={
+        isMobile
+          ? styles.mainContainerRecoverPassword1Mobile
+          : styles.mainContainerRecoverPassword1
+      }
+    >
+      <BackArrow executableFunction={() => setStep(0)} />
+      <div className={styles.icon}>
+        <PasswordIcon fontSize="inherit" />
+      </div>
+
+      <p className={styles.recoverPasswordText}>Recuperación de contraseña</p>
+      <div className={isMobile ? styles.singleInputMobile : styles.singleInput}>
+        <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
+          Correo Electrónico
+        </p>
+        <input
+          className={
+            isMobile
+              ? styles.singleInputContainerMobile
+              : styles.singleInputContainer
+          }
+          type="email"
+        />
+      </div>
+
+      <p
+        className={
+          isMobile
+            ? styles.willRecieveCodeTextMobile
+            : styles.willRecieveCodeText
+        }
+      >
+        Le enviaremos un correo electrónico con un código para restablecer su
+        contraseña.
+      </p>
+      <button className={styles.ingresarButton} onClick={() => setStep(2)}>
+        ENVIAR
+      </button>
+    </div>
+  );
 };
 
 export default LoginMainPage;
--- siaca-turnarounds-front/src/components/login/recoverPassword/RecoverPasswordStep2.tsx
+++ siaca-turnarounds-front/src/components/login/recoverPassword/RecoverPasswordStep2.tsx
@@ -8,59 +8,7 @@ interface PageProps {
 
 const LoginMainPage: React.FC<PageProps> = ({ setStep }) => {
   const isMobile = useMediaQuery("(max-width: 1270px)");
-  return (
-    <div
-      className={
-        isMobile
-          ? styles.mainContainerLoginMainPageMobile
-          : styles.mainContainerLoginMainPage
-      }
-    >
-      <Image src={SiacaLogo} alt="Logo" />
-      <p className={styles.welcomeBackText}>¡Bienvenido de vuelta!</p>
-      <div
-        className={
-          isMobile ? styles.inputsContainerMobile : styles.inputsContainer
-        }
-      >
-        <div
-          className={isMobile ? styles.singleInputMobile : styles.singleInput}
-        >
-          <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
-            Correo Electrónico
-          </p>
-          <input
-            className={
-              isMobile
-                ? styles.singleInputContainerMobile
-                : styles.singleInputContainer
-            }
-            type="email"
-          />
-        </div>
-        <div
-          className={isMobile ? styles.singleInputMobile : styles.singleInput}
-        >
-          <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
-            Contraseña
-          </p>
-          <input
-            className={
-              isMobile
-                ? styles.singleInputContainerMobile
-                : styles.singleInputContainer
-            }
-            type="password"
-          />
-        </div>
-      </div>
-
-      <button className={styles.ingresarButton}>INGRESAR</button>
-      <div className={styles.forgotPasswordText} onClick={() => setStep(1)}>
-        ¿Olvidaste tu contraseña?
-      </div>
-    </div>
-  );
+  return <div>step 2 recuperar contrasena</div>;
 };
 
 export default LoginMainPage;
