f993913aa7117cb3c8795425d7ec46c5e3747a27
mari1811 <maririos181@gmail.com>
2023-06-26 12:53:10 -0400

    Registro de usuario

--- siaca-turnarounds-front/src/components/login/register/RegisterStep1.tsx
+++ siaca-turnarounds-front/src/components/login/register/RegisterStep1.tsx
@@ -1,6 +1,9 @@
 import styles from "./RegisterStep1.style.module.css";
 import Image from "next/image";
 import { useMediaQuery } from "@mui/material";
+import MailLockIcon from '@mui/icons-material/MailLock';
+import { Input, Grid, Spacer } from "@nextui-org/react";
+
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -8,7 +11,61 @@ interface PageProps {
 
 const LoginMainPage: React.FC<PageProps> = ({ setStep }) => {
   const isMobile = useMediaQuery("(max-width: 1270px)");
-  return <div>register step 1</div>;
+  return (
+    <div
+      className={
+        isMobile
+          ? styles.mainContainerLoginMainPageMobile
+          : styles.mainContainerLoginMainPage
+      }
+    >
+      <MailLockIcon  sx={{ fontSize: 150 }}></MailLockIcon>
+      <strong><p className={styles.welcomeBackText}>Registro de usuario</p></strong>
+      <div
+        className={
+          isMobile ? styles.inputsContainerMobile : styles.inputsContainer
+        }
+      >
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Spacer y={1} />
+          <Grid>
+        <Input 
+          bordered 
+          labelPlaceholder="Correo Electrónico" 
+          color="success" 
+          width="375px"
+          />
+      </Grid>
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Spacer y={0.1} />
+          <Input.Password 
+          bordered 
+          labelPlaceholder="Contraseña"
+          color="success" 
+          width="375px"
+          />
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Spacer y={0.1} />
+          <Input.Password 
+          bordered 
+          labelPlaceholder="Confirmar contraseña"
+          color="success" 
+          width="375px"
+          />
+        </div>
+      </div>
+
+      <button className={styles.ingresarButton}  onClick={() => setStep(5)}><strong>CONTINUAR</strong></button>
+    </div>
+  );
 };
 
 export default LoginMainPage;
--- siaca-turnarounds-front/src/components/login/register/RegisterStep2.tsx
+++ siaca-turnarounds-front/src/components/login/register/RegisterStep2.tsx
@@ -1,6 +1,11 @@
 import styles from "./RegisterStep2.style.module.css";
+import React from "react";
 import Image from "next/image";
 import { useMediaQuery } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Dropdown } from "@nextui-org/react";
+import { Input, Grid } from "@nextui-org/react";
+
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -8,6 +13,11 @@ interface PageProps {
 
 const LoginMainPage: React.FC<PageProps> = ({ setStep }) => {
   const isMobile = useMediaQuery("(max-width: 1270px)");
+  const [selected, setSelected] = React.useState(new Set([" "]));
+  const selectedValue = React.useMemo(
+    () => Array.from(selected).join(", ").replaceAll("_", " "),
+    [selected]
+  );
   return (
     <div
       className={
@@ -16,49 +26,146 @@ const LoginMainPage: React.FC<PageProps> = ({ setStep }) => {
           : styles.mainContainerLoginMainPage
       }
     >
-      <Image src={SiacaLogo} alt="Logo" />
-      <p className={styles.welcomeBackText}>¡Bienvenido de vuelta!</p>
+      <AccountCircleIcon  sx={{ fontSize: 100 }}></AccountCircleIcon>
       <div
         className={
           isMobile ? styles.inputsContainerMobile : styles.inputsContainer
         }
       >
+        <strong><p className={isMobile ? styles.tituloMobile : styles.titulo}>Información Personal</p></strong>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+        <Grid>
+        <Input 
+          bordered 
+          labelLeft="Nombres" 
+          color="success"
+          width="375px"
+        />
+      </Grid>
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Grid>
+        <Input 
+          bordered 
+          labelLeft="Apellidos" 
+          color="success"
+          width="375px"
+        />
+      </Grid>
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Grid>
+        <Input 
+          bordered 
+          labelLeft="Cédula" 
+          color="success"
+          width="375px"
+        />
+      </Grid>
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <Grid>
+        <Input 
+          bordered 
+          labelLeft="Teléfono" 
+          color="success"
+          width="375px"
+        />
+      </Grid>
+        </div>
+
+        <strong><p className={isMobile ? styles.tituloMobile : styles.titulo}>Información Empresarial</p></strong>
         <div
           className={isMobile ? styles.singleInputMobile : styles.singleInput}
         >
           <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
-            Correo Electrónico
+            Departamento
           </p>
-          <input
-            className={
-              isMobile
-                ? styles.singleInputContainerMobile
-                : styles.singleInputContainer
-            }
-            type="email"
-          />
+        
+          <Dropdown>
+      <Dropdown.Button flat color="success" css={{ tt: "capitalize" }}>
+        {selectedValue}
+      </Dropdown.Button>
+      <Dropdown.Menu
+        aria-label="Single selection actions"
+        color="success"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKeys={selected}
+        onSelectionChange={setSelected}
+      >
+        <Dropdown.Item key="Opción 1">Opción 1</Dropdown.Item>
+        <Dropdown.Item key="Opción 2">Opción 2</Dropdown.Item>
+        <Dropdown.Item key="Opción 3">Opción 3</Dropdown.Item>
+        <Dropdown.Item key="Opción 4">Opción 4</Dropdown.Item>
+        <Dropdown.Item key="Opción 5">Opción 5</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+
         </div>
         <div
           className={isMobile ? styles.singleInputMobile : styles.singleInput}
         >
           <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
-            Contraseña
+            Cargo
           </p>
-          <input
-            className={
-              isMobile
-                ? styles.singleInputContainerMobile
-                : styles.singleInputContainer
-            }
-            type="password"
-          />
+          <Dropdown>
+      <Dropdown.Button flat color="success" css={{ tt: "capitalize" }}>
+        {selectedValue}
+      </Dropdown.Button>
+      <Dropdown.Menu
+        aria-label="Single selection actions"
+        color="success"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKeys={selected}
+        onSelectionChange={setSelected}
+      >
+        <Dropdown.Item key="Opción 1">Opción 1</Dropdown.Item>
+        <Dropdown.Item key="Opción 2">Opción 2</Dropdown.Item>
+        <Dropdown.Item key="Opción 3">Opción 3</Dropdown.Item>
+        <Dropdown.Item key="Opción 4">Opción 4</Dropdown.Item>
+        <Dropdown.Item key="Opción 5">Opción 5</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+        </div>
+        <div
+          className={isMobile ? styles.singleInputMobile : styles.singleInput}
+        >
+          <p className={isMobile ? styles.inputTextMobile : styles.inputText}>
+            Turno
+          </p>
+          <Dropdown>
+      <Dropdown.Button flat color="success" css={{ tt: "capitalize" }}>
+        {selectedValue}
+      </Dropdown.Button>
+      <Dropdown.Menu
+        aria-label="Single selection actions"
+        color="success"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKeys={selected}
+        onSelectionChange={setSelected}
+      >
+        <Dropdown.Item key="Opción 1">Opción 1</Dropdown.Item>
+        <Dropdown.Item key="Opción 2">Opción 2</Dropdown.Item>
+        <Dropdown.Item key="Opción 3">Opción 3</Dropdown.Item>
+        <Dropdown.Item key="Opción 4">Opción 4</Dropdown.Item>
+        <Dropdown.Item key="Opción 5">Opción 5</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
         </div>
       </div>
 
-      <button className={styles.ingresarButton}>INGRESAR</button>
-      <div className={styles.forgotPasswordText} onClick={() => setStep(1)}>
-        ¿Olvidaste tu contraseña?
-      </div>
+      <button className={styles.ingresarButton}  onClick={() => setStep(5)}><strong>REGISTRAR</strong></button>
     </div>
   );
 };
