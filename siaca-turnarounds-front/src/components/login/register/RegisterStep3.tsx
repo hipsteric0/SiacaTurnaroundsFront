5617cc949e56843839be4bfc30c2d8899b376254
mari1811 <maririos181@gmail.com>
2023-07-06 11:21:31 -0400

    Pantalla final de registro

--- siaca-turnarounds-front/src/components/login/register/RegisterStep3.tsx
+++ siaca-turnarounds-front/src/components/login/register/RegisterStep3.tsx
@@ -1,12 +1,36 @@
 import styles from "./RegisterStep3.style.module.css";
 import React, { useEffect, useState } from "react";
+import { useMediaQuery } from "@mui/material";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Spacer } from "@nextui-org/react";
 
-const LoginMainPage: React.FC = ({}) => {
+
+
+interface PageProps {
+  setStep: (value: number) => void;
+}
+
+const LoginMainPage: React.FC <PageProps> = ({setStep}) => {
+  const isMobile = useMediaQuery("(max-width: 1270px)");
   return (
-    <div>
-      Tu registro se ha realizado correctamente ya puedes hacer login .volver al
-      login boton
-    </div>
+
+    <div
+    className={
+      isMobile
+        ? styles.mainContainerLoginMainPageMobile
+        : styles.mainContainerLoginMainPage
+    }
+  >
+    <CheckCircleOutlineIcon sx={{ fontSize: 150 }}></CheckCircleOutlineIcon>
+    <strong>
+     <center> <p className={styles.welcomeBackText}>Tu registro se ha realizado correctamente ya puedes hacer login </p> </center>
+    </strong>
+    <Spacer y={2} />
+    <button className={styles.ingresarButton} onClick={() => setStep(0)}>
+        REGRESAR AL LOGIN
+      </button>
+ </div>
+
   );
 };
 
