735fd11b5f794820373848d59002c05481abf4fd
mari1811 <maririos181@gmail.com>
2023-07-28 19:50:24 -0400

    Lista de personal arreglo

--- siaca-turnarounds-front/src/components/Personnel/PersonnelMainPage.tsx
+++ siaca-turnarounds-front/src/components/Personnel/PersonnelMainPage.tsx
@@ -24,44 +24,7 @@ const PersonnelMainPage: React.FC = () => {
   const isMobile = useMediaQuery("(max-width: 1270px)");
   const [allowContinue, setAllowContinue] = useState(false);
   const [arrayList3, setArrayList3] = useState([]);
-  const [arrayList3length, setArrayList3length] = useState(-1);
-  const [arrayListData3, setArrayListData3] = useState(
-    <Table.Row key={1}>
-      <Table.Cell>cargando</Table.Cell>
-      <Table.Cell>asdas</Table.Cell>
-      <Table.Cell>asd</Table.Cell>
-      <Table.Cell>asdasd</Table.Cell>
-      <Table.Cell>asdas</Table.Cell>
-      <Table.Cell>
-        <RemoveRedEyeIcon /> <BorderColorOutlinedIcon />{" "}
-        <DeleteOutlineOutlinedIcon />{" "}
-      </Table.Cell>
-    </Table.Row>
-  );
-  let auxRows = (
-    <Table.Row key={1}>
-      <Table.Cell>cargando</Table.Cell>
-      <Table.Cell>asdas</Table.Cell>
-      <Table.Cell>asd</Table.Cell>
-      <Table.Cell>asdasd</Table.Cell>
-      <Table.Cell>asdas</Table.Cell>
-      <Table.Cell>
-        <RemoveRedEyeIcon /> <BorderColorOutlinedIcon />{" "}
-        <DeleteOutlineOutlinedIcon />{" "}
-      </Table.Cell>
-    </Table.Row>
-  );
 
-  const [selectedDepartment, setSelectedDepartment] = React.useState(
-    new Set([" "])
-  );
-  const selectedDepartmentValue = React.useMemo(
-    () => Array.from(selectedDepartment).join(", ").replaceAll("_", " "),
-    [selectedDepartment]
-  );
-
-  let arrayList: any = [];
-  let h: any;
 
   useEffect(() => {
     getList();
@@ -81,7 +44,6 @@ const PersonnelMainPage: React.FC = () => {
           res.json().then((result) => {
             console.log(result);
             console.log("values", Object.values(result));
-            arrayList = Object.values(result);
 
             setArrayList3(Object.values(result));
           })
@@ -92,128 +54,46 @@ const PersonnelMainPage: React.FC = () => {
       }
     };
     await fetchData().catch(console.error);
-    h = arrayPrinter();
+
   };
 
   const arrayPrinter = () => {
     let y: any = [];
     console.log("arrayList3", arrayList3.length);
     arrayList3.map((index: any) => {
-      console.log("index", index);
-      console.log("index?.fk_user.first_name", index.fk_user.first_name);
-      console.log("index.cargo", index.cargo);
       y[index.id] = (
-        <Table.Row key={index.id}>
-          <Table.Cell>
-            {index.fk_user.first_name} {index.fk_user.last_name}{" "}
-          </Table.Cell>
-          <Table.Cell>{index.cargo}</Table.Cell>
-          <Table.Cell>{index.departamento}</Table.Cell>
-          <Table.Cell>
-            {index.fk_user.username} - {index.telefono}
-          </Table.Cell>
-          <Table.Cell>{index.turno}</Table.Cell>
-          <Table.Cell>
-            <RemoveRedEyeIcon /> <BorderColorOutlinedIcon />{" "}
-            <DeleteOutlineOutlinedIcon />{" "}
-          </Table.Cell>
-        </Table.Row>
+        <tr key={index.id}>
+          <td>{index.fk_user.first_name} {index.fk_user.last_name}</td>
+          <td>{index.cargo}</td>
+          <td>{index.departamento}</td>
+          <td>{index.fk_user.username} - {index.telefono}</td>
+          <td>{index.turno}</td>
+        </tr>
       );
     });
-    console.log("arrayList", Symbol(arrayList[0]?.cargo).description);
-    let sas = arrayList[0]?.cargo;
-    let x = (
-      <Table.Row key={1}>
-        <Table.Cell>{Symbol(arrayList[0]?.cargo).description}</Table.Cell>
-        <Table.Cell>asdas</Table.Cell>
-        <Table.Cell>asd</Table.Cell>
-        <Table.Cell>asdasd</Table.Cell>
-        <Table.Cell>asdas</Table.Cell>
-        <Table.Cell>
-          <RemoveRedEyeIcon /> <BorderColorOutlinedIcon />{" "}
-          <DeleteOutlineOutlinedIcon />{" "}
-        </Table.Cell>
-      </Table.Row>
-    );
-    setArrayListData3(y);
+ 
     return y;
   };
 
   return (
     <main className={styles.containerPersonnelMainPage}>
-      length: {arrayList3.length}
-      <button onClick={() => setArrayList3length(arrayList3length + 1)}>
-        {" "}
-        ver personal
-      </button>
       <Spacer />
       <div className={styles.personnelListContainer}>
-        <Table
-          lined
-          headerLined
-          shadow={false}
-          aria-label="Example static collection table"
-          css={{
-            height: "auto",
-            minWidth: "100%",
-          }}
-        >
-          <Table.Header>
-            <Table.Column>Nombre ({arrayList3length})</Table.Column>
-            <Table.Column>Cargo</Table.Column>
-            <Table.Column>Departamento</Table.Column>
-            <Table.Column>Contacto</Table.Column>
-            <Table.Column>Turno</Table.Column>
-            <Table.Column>Opciones</Table.Column>
-          </Table.Header>
-          {arrayList3length > 0 ? (
-            <Table.Body>{arrayListData3}</Table.Body>
-          ) : (
-            <Table.Body>{auxRows}</Table.Body>
-          )}
-        </Table>
+      <tbody>
+        <tr>
+          <th>Nombre</th>
+          <th>Cargo</th>
+          <th>Departamento</th>
+          <th>Contacto</th>
+          <th>Turno</th>
+        </tr>
+      {arrayPrinter()}
+      </tbody>
       </div>
       {!allowContinue}
-      {arrayListData3.key}
-      {arrayList3.length}
     </main>
   );
 };
 
 export default PersonnelMainPage;
 
-let PersonnelArray: any = [];
-
-let arrayAux = [
-  {
-    id: 0,
-    first_name: "Nombre",
-    last_name: "Apellido",
-    username: "correo@gmail.com",
-    cargo: "Cargo",
-    departamento: "Departamento",
-    telefono: "48374783784",
-    turno: "diurno",
-  },
-
-  {
-    id: 1,
-    first_name: "Nombre",
-    last_name: "Apellido",
-    username: "correo@gmail.com",
-    cargo: "Cargo",
-    departamento: "Departamento",
-    telefono: "48374783784",
-    turno: "diurno",
-  },
-  {
-    id: 2,
-    first_name: "Nombre",
-    last_name: "Apellido",
-    username: "correo@gmail.com",
-    cargo: "Cargo",
-    departamento: "Departamento",
-    telefono: "48374783784",
-    turno: "diurno",
-  },
-];
