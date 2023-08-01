c5c06e41694b031ae92f1f48d5de04a8ebce2e3f
mari1811 <maririos181@gmail.com>
2023-08-01 16:23:59 -0400

    Lista de aerolineas

--- siaca-turnarounds-front/src/components/airlines/AirlinesMainPage.tsx
+++ siaca-turnarounds-front/src/components/airlines/AirlinesMainPage.tsx
@@ -3,10 +3,16 @@ import styles from "./AirlinesMainPage.style.module.css";
 import KeyboardArrowLeftRoundedIcon from "@mui/icons-material/KeyboardArrowLeftRounded";
 import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 import { log } from "console";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import router from "next/router";
-import { Table } from "@nextui-org/react";
+import { Table, Spacer } from "@nextui-org/react";
 import { TableBody } from "@mui/material";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import { Dropdown } from "@nextui-org/react";
+import { useMediaQuery } from "@mui/material";
+import { text } from "stream/consumers";
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -14,30 +20,58 @@ interface PageProps {
 
 const AirlinesMainPage: React.FC<PageProps> = ({ setStep }) => {
   //if token exists show regular html else show not signed in screen
-  let tableHTML = (
-    <Table.Row key={1}>
-      <Table.Cell>Tony Reichert</Table.Cell>
-      <Table.Cell>CEO</Table.Cell>
-      <Table.Cell>Active</Table.Cell>
-      <Table.Cell>CEO</Table.Cell>
-      <Table.Cell>Active</Table.Cell>
-    </Table.Row>
-  );
+  const isMobile = useMediaQuery("(max-width: 1270px)");
+  const [allowContinue, setAllowContinue] = useState(false);
+  const [arrayList3, setArrayList3] = useState([]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = async () => {
+    const fetchData = async () => {
+      try {
+        const url = "/api/airlinesList";
+        const requestOptions = {
+          method: "POST",
+          body: JSON.stringify({
+            userToken: localStorage.getItem("userToken"),
+          }),
+        };
+        const response = await fetch(url, requestOptions).then((res) =>
+          res.json().then((result) => {
+            console.log(result);
+            console.log("values", Object.values(result));
+
+            setArrayList3(Object.values(result));
+          })
+        );
+      } catch (error) {
+        console.error("Error geting user", error);
+        return;
+      }
+    };
+    await fetchData().catch(console.error);
+  };
+
   const arrayPrinter = () => {
     let y: any = [];
-    arrayAux.map((index: any) => {
-      return (y[index.id] = (
-        <Table.Row key={index?.id}>
-          <Table.Cell>{index?.imagen}</Table.Cell>
-          <Table.Cell>{index?.nombre}</Table.Cell>
-          <Table.Cell>{index?.correo}</Table.Cell>
-          <Table.Cell>{index?.telefono}</Table.Cell>
-          <Table.Cell>{index?.codigo}</Table.Cell>
-        </Table.Row>
-      ));
+    console.log("arrayList3", arrayList3.length);
+    arrayList3.map((index: any) => {
+      y[index.id] = (
+        <div key={index.id} className={styles.tableInfoRow}>
+          <td><img/></td>
+          <td>{index.nombre}</td>
+          <td>{index.correo}</td>
+          <td>{index.telefono}</td>
+          <td>{index.codigo}</td>
+        </div>
+      );
     });
+
     return y;
   };
+  
   return (
     <main className={styles.containerAirlinesMainPage}>
       <div className={styles.registerbuttoncontainer}>
@@ -47,26 +81,17 @@ const AirlinesMainPage: React.FC<PageProps> = ({ setStep }) => {
         />
       </div>
       <div className={styles.airlinesListContainer}>
-        <Table
-          lined
-          headerLined
-          shadow={false}
-          aria-label="Example static collection table"
-          css={{
-            height: "auto",
-            minWidth: "100%",
-          }}
-        >
-          <Table.Header>
-            <Table.Column> </Table.Column>
-            <Table.Column>Aerolínea</Table.Column>
-            <Table.Column>Correo</Table.Column>
-            <Table.Column>Teléfono</Table.Column>
-            <Table.Column>Código</Table.Column>
-          </Table.Header>
-          <Table.Body>{arrayPrinter()}</Table.Body>
-        </Table>
-      </div>
+        <div>
+          <div className={styles.tableTitlesContainer}>
+            <span>Logo</span>
+            <span>Nombre</span>
+            <span>Correo</span>
+            <span>Teléfono</span>
+            <span>Código</span>
+          </div>
+          {arrayPrinter()}
+        </div>
+        </div>
     </main>
   );
 };
