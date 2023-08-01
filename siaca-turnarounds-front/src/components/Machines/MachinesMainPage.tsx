890b5bbb1eb3e072844919b5fda2dbae05a17043
mari1811 <maririos181@gmail.com>
2023-08-01 18:43:02 -0400

    Tarjetas de maquinaria

--- siaca-turnarounds-front/src/components/Machines/MachinesMainPage.tsx
+++ siaca-turnarounds-front/src/components/Machines/MachinesMainPage.tsx
@@ -13,7 +13,7 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 import { Dropdown } from "@nextui-org/react";
 import { useMediaQuery } from "@mui/material";
 import { Collapse, Text } from "@nextui-org/react";
-
+import {Card, Image} from "@nextui-org/react";
 
 
 
@@ -25,7 +25,61 @@ const MachinesMainPage: React.FC = () => {
   //if token exists show regular html else show not signed in screen
 
   const isMobile = useMediaQuery("(max-width: 1270px)");
+  const [allowContinue, setAllowContinue] = useState(false);
+  const [arrayList3, setArrayList3] = useState([]);
 
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = async () => {
+    const fetchData = async () => {
+      try {
+        const url = "/api/machinesList";
+        const requestOptions = {
+          method: "POST",
+          body: JSON.stringify({
+            userToken: localStorage.getItem("userToken"),
+          }),
+        };
+        const response = await fetch(url, requestOptions).then((res) =>
+          res.json().then((result) => {
+            console.log(result);
+            console.log("values", Object.values(result));
+
+            setArrayList3(Object.values(result));
+          })
+        );
+      } catch (error) {
+        console.error("Error geting user", error);
+        return;
+      }
+    };
+    await fetchData().catch(console.error);
+  };
+
+  const arrayPrinter = () => {
+    let y: any = [];
+    console.log("arrayList3", arrayList3.length);
+    arrayList3.map((index: any) => {
+      y[index.id] = (
+
+        
+          <div className={styles.machinescard }key={index.id}>
+            <div className={styles.machinesheader}>
+            </div>
+          <h3 className={styles.h3}>{index.identificador}</h3>
+          <p className={styles.p}>{index.modelo}</p>
+          <p className={styles.p}>{index.combustible}</p>
+          <p className={styles.p}>{index.estado}</p>
+          </div>
+
+          
+      );
+    });
+
+    return y;
+  };
 
   return (
     <main className={styles.containerMachineMainPage}>
@@ -33,9 +87,9 @@ const MachinesMainPage: React.FC = () => {
       <div className={styles.machineListContainer}>
       <Collapse.Group>
       <Collapse title="Aguas Servidas">
-        <Text>
-          MAQUINARIAS
-        </Text>
+      <div className={styles.machinesgrid}>
+        {arrayPrinter()}
+        </div>
       </Collapse>
       <Collapse title="Tractor de arrastre">
         <Text>
