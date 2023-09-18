9762dec632c1fb3518ad71bf08bdab696c44fb6e
hipsteric0 <aleromerosurf@gmail.com>
2023-09-18 15:16:00 -0400

    fixes to solicitudes

--- siaca-turnarounds-front/src/components/Personnel/PersonnelMainPage.tsx
+++ siaca-turnarounds-front/src/components/Personnel/PersonnelMainPage.tsx
@@ -5,8 +5,8 @@ import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRigh
 import { log } from "console";
 import React, { useEffect, useState } from "react";
 import router from "next/router";
-import { Table, Spacer} from "@nextui-org/react";
-import { TableBody, Dialog} from "@mui/material";
+import { Table, Spacer } from "@nextui-org/react";
+import { TableBody, Dialog } from "@mui/material";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
@@ -15,11 +15,11 @@ import { useMediaQuery } from "@mui/material";
 import { text } from "stream/consumers";
 import Combobox from "../Reusables/Combobox";
 import RedButton2 from "../Reusables/RedButton2";
-import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 import GreenButton from "../Reusables/GreenButton";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import BlockIcon from '@mui/icons-material/Block';
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import BlockIcon from "@mui/icons-material/Block";
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -39,7 +39,7 @@ const PersonnelMainPage: React.FC = () => {
   const [denyDialog, setDenyDialog] = useState(false);
   const [aceptDialog, setAceptDialog] = useState(false);
   const [listDialog, setListDialog] = useState(false);
-  const [solicitudeCounter, setSolicitudeCounter ] = useState(-1);
+  const [solicitudeCounter, setSolicitudeCounter] = useState(-1);
   let filterValues: any[] = [];
 
   useEffect(() => {
@@ -178,7 +178,6 @@ const PersonnelMainPage: React.FC = () => {
         };
         const response = await fetch(url, requestOptions).then((res) =>
           res.json().then((result) => {
-            console.log("CONTADOR", result.contador);
             setSolicitudeCounter(result.contador);
           })
         );
@@ -189,7 +188,7 @@ const PersonnelMainPage: React.FC = () => {
     };
     await fetchData().catch(console.error);
   };
-  
+
   const arrayPrinter = () => {
     let y: any = [];
     let arrayList3aux: any = [];
@@ -206,33 +205,38 @@ const PersonnelMainPage: React.FC = () => {
         <div key={index.id} className={styles.tableInfoRow}>
           {
             <Dialog
-            className={styles.dialogDelete}
-            open={deleteDialog}
-            onClose={() => setDeleteDialog(false)}
-          >
-            <div className={styles.dialogBack}>
-            <div className={styles.dialogText}>
-              <div className={styles.warningIcon}><WarningAmberIcon color="warning" fontSize="inherit"/></div>
-              <p><strong>¿Está seguro que desea eliminar este usuario?</strong></p>
-              <br/>
-              <div className={styles.dialogButtons}>
-              <GreenButton2
-                executableFunction={() => {
-                handleDeletePersonnel(clickID);
-                }}
-                buttonText="Si"
-              />
-              <RedButton2
-                executableFunction={() => {
-                  setDeleteDialog(false);
-                }}
-                buttonText="No"
-              />
+              className={styles.dialogDelete}
+              open={deleteDialog}
+              onClose={() => setDeleteDialog(false)}
+            >
+              <div className={styles.dialogBack}>
+                <div className={styles.dialogText}>
+                  <div className={styles.warningIcon}>
+                    <WarningAmberIcon color="warning" fontSize="inherit" />
+                  </div>
+                  <p>
+                    <strong>
+                      ¿Está seguro que desea eliminar este usuario?
+                    </strong>
+                  </p>
+                  <br />
+                  <div className={styles.dialogButtons}>
+                    <GreenButton2
+                      executableFunction={() => {
+                        handleDeletePersonnel(clickID);
+                      }}
+                      buttonText="Si"
+                    />
+                    <RedButton2
+                      executableFunction={() => {
+                        setDeleteDialog(false);
+                      }}
+                      buttonText="No"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            </div>
-          </Dialog>
-
+            </Dialog>
           }
 
           <td>
@@ -241,7 +245,7 @@ const PersonnelMainPage: React.FC = () => {
           <td>{index?.cargo}</td>
           <td>{index?.departamento}</td>
           <td>
-            {index?.fk_user?.username} 
+            {index?.fk_user?.username}
             <p>{index?.telefono}</p>
           </td>
           <td>{index?.turno}</td>
@@ -251,45 +255,46 @@ const PersonnelMainPage: React.FC = () => {
               onMouseEnter={() => {
                 sethoverEyeId(index.id);
               }}
-              onMouseLeave={() => {              
+              onMouseLeave={() => {
                 sethoverEyeId(-1);
               }}
             >
               <RemoveRedEyeIcon
-              htmlColor={hoverEyeId === index.id ? "#00A75D" : "#4D4E56"}
-              onClick={() => {
-                
-              }}/>{" "}
+                htmlColor={hoverEyeId === index.id ? "#00A75D" : "#4D4E56"}
+                onClick={() => {}}
+              />{" "}
             </div>
 
-            <div className={styles.functionIcon}
-            onMouseEnter={() => {
-              sethoverPencilId(index.id);
-            }}
-            onMouseLeave={() => {              
-              sethoverPencilId(-1);
-            }}>
-              <BorderColorOutlinedIcon 
-              htmlColor={hoverPencilId === index.id ? "#00A75D" : "#4D4E56"}
-              onClick={() => {
-                
-                
-              }}/>{" "}
+            <div
+              className={styles.functionIcon}
+              onMouseEnter={() => {
+                sethoverPencilId(index.id);
+              }}
+              onMouseLeave={() => {
+                sethoverPencilId(-1);
+              }}
+            >
+              <BorderColorOutlinedIcon
+                htmlColor={hoverPencilId === index.id ? "#00A75D" : "#4D4E56"}
+                onClick={() => {}}
+              />{" "}
             </div>
 
-            <div className={styles.functionIcon}
-            onMouseEnter={() => {
-              sethoverTrashId(index.id);
-            }}
-            onMouseLeave={() => {              
-              sethoverTrashId(-1);
-            }}>
+            <div
+              className={styles.functionIcon}
+              onMouseEnter={() => {
+                sethoverTrashId(index.id);
+              }}
+              onMouseLeave={() => {
+                sethoverTrashId(-1);
+              }}
+            >
               <DeleteOutlineOutlinedIcon
-               htmlColor={hoverTrashId === index.id ? "#f10303" : "#4D4E56"}
-               onClick={() => {
-                setclickID(index.fk_user.id);
-                setDeleteDialog(true);
-               }}
+                htmlColor={hoverTrashId === index.id ? "#f10303" : "#4D4E56"}
+                onClick={() => {
+                  setclickID(index.fk_user.id);
+                  setDeleteDialog(true);
+                }}
               />
             </div>
           </td>
@@ -316,64 +321,77 @@ const PersonnelMainPage: React.FC = () => {
         <div key={index.id} className={styles.tableInfoRow}>
           {
             <Dialog
-            className={styles.dialogDelete}
-            open={aceptDialog}
-            onClose={() => setAceptDialog(false)}
-          >
-            <div className={styles.dialogBack}>
-            <div className={styles.dialogText}>
-              <div className={styles.warningIcon}><CheckCircleOutlineIcon color="success" fontSize="inherit"/></div>
-              <p><strong>¿Está seguro que desea aceptar este usuario?</strong></p>
-              <br/>
-              <div className={styles.dialogButtons}>
-              <GreenButton2
-                executableFunction={() => {
-                changePersonnelState(clickID2)
-                }}
-                buttonText="Si"
-              />
-              <RedButton2
-                executableFunction={() => {
-                  setAceptDialog(false);
-                }}
-                buttonText="No"
-              />
+              className={styles.dialogDelete}
+              open={aceptDialog}
+              onClose={() => setAceptDialog(false)}
+            >
+              <div className={styles.dialogBack}>
+                <div className={styles.dialogText}>
+                  <div className={styles.warningIcon}>
+                    <CheckCircleOutlineIcon
+                      color="success"
+                      fontSize="inherit"
+                    />
+                  </div>
+                  <p>
+                    <strong>
+                      ¿Está seguro que desea aceptar este usuario?
+                    </strong>
+                  </p>
+                  <br />
+                  <div className={styles.dialogButtons}>
+                    <GreenButton2
+                      executableFunction={() => {
+                        changePersonnelState(clickID2);
+                      }}
+                      buttonText="Si"
+                    />
+                    <RedButton2
+                      executableFunction={() => {
+                        setAceptDialog(false);
+                      }}
+                      buttonText="No"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            </div>
-          </Dialog>
-
+            </Dialog>
           }
-          
+
           {
             <Dialog
-            className={styles.dialogDelete}
-            open={denyDialog}
-            onClose={() => setDenyDialog(false)}
-          >
-            <div className={styles.dialogBack}>
-            <div className={styles.dialogText}>
-              <div className={styles.warningIcon}><BlockIcon color="error" fontSize="inherit"/></div>
-              <p><strong>¿Está seguro que desea rechazar a este usuario?</strong></p>
-              <br/>
-              <div className={styles.dialogButtons}>
-              <GreenButton2
-                executableFunction={() => {
-                handleDeletePersonnel(clickID2);
-                }}
-                buttonText="Si"
-              />
-              <RedButton2
-                executableFunction={() => {
-                  setDenyDialog(false);
-                }}
-                buttonText="No"
-              />
+              className={styles.dialogDelete}
+              open={denyDialog}
+              onClose={() => setDenyDialog(false)}
+            >
+              <div className={styles.dialogBack}>
+                <div className={styles.dialogText}>
+                  <div className={styles.warningIcon}>
+                    <BlockIcon color="error" fontSize="inherit" />
+                  </div>
+                  <p>
+                    <strong>
+                      ¿Está seguro que desea rechazar a este usuario?
+                    </strong>
+                  </p>
+                  <br />
+                  <div className={styles.dialogButtons}>
+                    <GreenButton2
+                      executableFunction={() => {
+                        handleDeletePersonnel(clickID2);
+                      }}
+                      buttonText="Si"
+                    />
+                    <RedButton2
+                      executableFunction={() => {
+                        setDenyDialog(false);
+                      }}
+                      buttonText="No"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            </div>
-          </Dialog>
-
+            </Dialog>
           }
 
           <td>
@@ -382,7 +400,7 @@ const PersonnelMainPage: React.FC = () => {
           <td>{index?.cargo}</td>
           <td>{index?.departamento}</td>
           <td>
-            {index?.fk_user?.username} 
+            {index?.fk_user?.username}
             <p>{index?.telefono}</p>
           </td>
           <td>{index?.turno}</td>
@@ -392,31 +410,34 @@ const PersonnelMainPage: React.FC = () => {
               onMouseEnter={() => {
                 sethoverEyeId(index.id);
               }}
-              onMouseLeave={() => {              
+              onMouseLeave={() => {
                 sethoverEyeId(-1);
               }}
             >
               <CheckCircleOutlineIcon
-              htmlColor={hoverEyeId === index.id ? "#00A75D" : "#4D4E56"}
-              onClick={() => {
-                setclickID2(index.fk_user.id);
-                setAceptDialog(true);
-              }}/>{" "}
+                htmlColor={hoverEyeId === index.id ? "#00A75D" : "#4D4E56"}
+                onClick={() => {
+                  setclickID2(index.fk_user.id);
+                  setAceptDialog(true);
+                }}
+              />{" "}
             </div>
 
-            <div className={styles.functionIcon}
-            onMouseEnter={() => {
-              sethoverTrashId(index.id);
-            }}
-            onMouseLeave={() => {              
-              sethoverTrashId(-1);
-            }}>
+            <div
+              className={styles.functionIcon}
+              onMouseEnter={() => {
+                sethoverTrashId(index.id);
+              }}
+              onMouseLeave={() => {
+                sethoverTrashId(-1);
+              }}
+            >
               <BlockIcon
-               htmlColor={hoverTrashId === index.id ? "#f10303" : "#4D4E56"}
-               onClick={() => {
-                setclickID2(index.fk_user.id);
-                setDenyDialog(true);
-               }}
+                htmlColor={hoverTrashId === index.id ? "#f10303" : "#4D4E56"}
+                onClick={() => {
+                  setclickID2(index.fk_user.id);
+                  setDenyDialog(true);
+                }}
               />
             </div>
           </td>
@@ -426,58 +447,63 @@ const PersonnelMainPage: React.FC = () => {
     return y;
   };
 
-
   return (
     <main className={styles.containerPersonnelMainPage}>
       <Spacer />
 
       {
-            <Dialog
-            className={styles.dialogList}
-            open={listDialog}
-            onClose={() => setListDialog(false)}
-            fullScreen={true}
-          >
-            <div>
+        <Dialog
+          className={styles.dialogList}
+          open={listDialog}
+          onClose={() => setListDialog(false)}
+          fullScreen={true}
+        >
+          <div>
             <div
               className={styles.closeIconDialog}
               onClick={() => setListDialog(false)}
-                >
+            >
               <CloseRoundedIcon htmlColor="#4d4e56" />
             </div>
           </div>
 
-            <div className={styles.personnelList}>
-        <div>
-          <div className={styles.tableTitlesContainer}>
-            <span>Nombre</span>
-            <span>Cargo</span>
-            <span>Departamento</span>
-            <span>Contacto</span>
-            <span>Turno</span>
-            <span>Opciones</span>
+          <div className={styles.personnelListDialogContainer}>
+            <div className={styles.personnelListDialog}>
+              <div>
+                <div className={styles.tableTitlesContainer}>
+                  <span>Nombre</span>
+                  <span>Cargo</span>
+                  <span>Departamento</span>
+                  <span>Contacto</span>
+                  <span>Turno</span>
+                  <span>Opciones</span>
+                </div>
+                {arrayPrinter2()}
+              </div>
+            </div>
           </div>
-          {arrayPrinter2()}
+
+          {!allowContinue}
+        </Dialog>
+      }
+
+      <div className={styles.upperSectionContainer}>
+        <div>
+          <div className={styles.comboboxMainContainer}>
+            <Combobox
+              onClickFilteringFunction={filterArray}
+              setIsFiltered={setIsFilteredResults}
+              filterValues={filterValues}
+            />
+          </div>
         </div>
-      </div>
-      {!allowContinue}
-
-          </Dialog>
-
-          }
-
-      <div className={styles.comboboxMainContainer}>
-        <Combobox
-          onClickFilteringFunction={filterArray}
-          setIsFiltered={setIsFilteredResults}
-          filterValues={filterValues}
-        />
-
+        <div className={styles.greenbuttonSuperContainer}>
           <GreenButton
-          executableFunction={() => setListDialog(true)}
-          buttonText="Solicitudes" 
-        />
-
+            executableFunction={() => setListDialog(true)}
+            buttonText={"Solicitudes" + " (" + solicitudeCounter + ")"}
+            disabled={solicitudeCounter < 1}
+          />
+        </div>
       </div>
 
       <div className={styles.personnelListContainer}>
