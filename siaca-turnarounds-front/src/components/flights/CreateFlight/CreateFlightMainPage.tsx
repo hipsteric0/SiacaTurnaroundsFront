bb7201668f1fe1b230a2642a468598c6a5734548
hipsteric0 <aleromerosurf@gmail.com>
2023-09-21 18:13:19 -0400

    autocomplete working create flight

--- siaca-turnarounds-front/src/components/flights/CreateFlight/CreateFlightMainPage.tsx
+++ siaca-turnarounds-front/src/components/flights/CreateFlight/CreateFlightMainPage.tsx
@@ -6,7 +6,7 @@ import { log } from "console";
 import React, { useEffect, useState } from "react";
 import router from "next/router";
 import { Table, Spacer } from "@nextui-org/react";
-import { TableBody } from "@mui/material";
+import { Autocomplete, TableBody, TextField } from "@mui/material";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
@@ -18,6 +18,7 @@ import DropdownMenu from "@/components/Reusables/DropdownMenu";
 import StandardInput from "@/components/Reusables/StandardInput";
 import RedButton from "@/components/Reusables/RedButton";
 import { Suspense } from "react";
+import StandardInputAutocomplete from "@/components/Reusables/StandardInputAutocomplete";
 
 interface PageProps {
   setStep: (value: number) => void;
@@ -34,6 +35,7 @@ const CreateFlightMainPage: React.FC<PageProps> = ({ setStep }) => {
   const [DepartureCitiesArrayList, setDepartureCitiesArrayList] = useState([]);
   const [AirlinesArrayList, setAirlinesArrayList] = useState([]);
   const [FlightTypesList, setFlightTypesList] = useState([]);
+  const [ACRegsList, setACRegsList] = useState([]);
 
   const [FlightServiceType, setFlightServiceType] = useState("");
   const [STN, setSTN] = useState("");
@@ -63,6 +65,8 @@ const CreateFlightMainPage: React.FC<PageProps> = ({ setStep }) => {
   const [recurrentSaturday, setrecurrentSaturday] = useState(false);
   const [recurrentSunday, setrecurrentSunday] = useState(false);
 
+  const [acRegInput, setacRegInput] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       await getcitiesListDestination();
@@ -71,9 +75,46 @@ const CreateFlightMainPage: React.FC<PageProps> = ({ setStep }) => {
       await getFlightTypesList();
       await getTemplatesList();
       await getFlightServiceTypes();
+      await getACRegList();
     };
     fetchData().catch(console.error);
   }, []);
+
+  const setACRegListDropdown = async (ACRegsListArray: []) => {
+    if (acregOptionsArray.length === 0) {
+      ACRegsListArray.map((index: any) => {
+        acregOptionsArray.push({
+          id_airline: index?.fk_aerolinea_id,
+          title: index?.ac_reg,
+        });
+      });
+    }
+  };
+  const getACRegList = async () => {
+    const fetchData = async () => {
+      try {
+        const url = "/api/acRegByAirline";
+        const requestOptions = {
+          method: "POST",
+          body: JSON.stringify({
+            userToken: localStorage.getItem("userToken"),
+          }),
+        };
+        const response = await fetch(url, requestOptions).then((res) =>
+          res.json().then(async (result) => {
+            console.log("ACREGSList", Object.values(result));
+            await setFlightTypesList(Object.values(result));
+
+            await setACRegListDropdown(Object.values(result));
+          })
+        );
+      } catch (error) {
+        console.error("Error geting user", error);
+        return;
+      }
+    };
+    await fetchData().catch(console.error);
+  };
 
   const registerTurnaround = async (
     fecha_inicioValue: string,
@@ -550,11 +591,40 @@ const CreateFlightMainPage: React.FC<PageProps> = ({ setStep }) => {
               </div>
               <div className={styles.dataContainerRowItem}>
                 <p>A/C Reg:</p>
-                <StandardInput
-                  setValue={setACreg}
-                  inputText=""
-                  inputWidth="185px"
-                />
+                {acRegInput ? (
+                  <StandardInput
+                    setValue={setACreg}
+                    inputText=""
+                    inputWidth="185px"
+                  />
+                ) : (
+                  <div className={styles.autocompleteInputContainer}>
+                    <Autocomplete
+                      id="free-solo-demo"
+                      freeSolo
+                      fullWidth
+                      options={acregOptionsArray?.map((option) => {
+                        if (Carrier > 0) {
+                          if (option?.id_airline === Carrier) {
+                            return option?.title;
+                          } else {
+                            return "";
+                          }
+                        } else {
+                          return "";
+                        }
+                      })}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          size="small"
+                          label=""
+                          onChange={({ target: { value } }) => setACreg(value)}
+                        />
+                      )}
+                    />
+                  </div>
+                )}
               </div>
               <div className={styles.dataContainerRowItem}>
                 <p>A/C type:</p>
@@ -820,4 +890,132 @@ let optionsArray: any = [
     key: 4,
     name: "Imagen",
   },
+];
+let acregOptionsArray: any = [];
+const top100Films = [
+  { title: "The Shawshank Redemption" },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
+  { title: "The Good, the Bad and the Ugly", year: 1966 },
+  { title: "Fight Club", year: 1999 },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001,
+  },
+  {
+    title: "Star Wars: Episode V - The Empire Strikes Back",
+    year: 1980,
+  },
+  { title: "Forrest Gump", year: 1994 },
+  { title: "Inception", year: 2010 },
+  {
+    title: "The Lord of the Rings: The Two Towers",
+    year: 2002,
+  },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: "Goodfellas", year: 1990 },
+  { title: "The Matrix", year: 1999 },
+  { title: "Seven Samurai", year: 1954 },
+  {
+    title: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+  },
+  { title: "City of God", year: 2002 },
+  { title: "Se7en", year: 1995 },
+  { title: "The Silence of the Lambs", year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: "Life Is Beautiful", year: 1997 },
+  { title: "The Usual Suspects", year: 1995 },
+  { title: "Léon: The Professional", year: 1994 },
+  { title: "Spirited Away", year: 2001 },
+  { title: "Saving Private Ryan", year: 1998 },
+  { title: "Once Upon a Time in the West", year: 1968 },
+  { title: "American History X", year: 1998 },
+  { title: "Interstellar", year: 2014 },
+  { title: "Casablanca", year: 1942 },
+  { title: "City Lights", year: 1931 },
+  { title: "Psycho", year: 1960 },
+  { title: "The Green Mile", year: 1999 },
+  { title: "The Intouchables", year: 2011 },
+  { title: "Modern Times", year: 1936 },
+  { title: "Raiders of the Lost Ark", year: 1981 },
+  { title: "Rear Window", year: 1954 },
+  { title: "The Pianist", year: 2002 },
+  { title: "The Departed", year: 2006 },
+  { title: "Terminator 2: Judgment Day", year: 1991 },
+  { title: "Back to the Future", year: 1985 },
+  { title: "Whiplash", year: 2014 },
+  { title: "Gladiator", year: 2000 },
+  { title: "Memento", year: 2000 },
+  { title: "The Prestige", year: 2006 },
+  { title: "The Lion King", year: 1994 },
+  { title: "Apocalypse Now", year: 1979 },
+  { title: "Alien", year: 1979 },
+  { title: "Sunset Boulevard", year: 1950 },
+  {
+    title:
+      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    year: 1964,
+  },
+  { title: "The Great Dictator", year: 1940 },
+  { title: "Cinema Paradiso", year: 1988 },
+  { title: "The Lives of Others", year: 2006 },
+  { title: "Grave of the Fireflies", year: 1988 },
+  { title: "Paths of Glory", year: 1957 },
+  { title: "Django Unchained", year: 2012 },
+  { title: "The Shining", year: 1980 },
+  { title: "WALL·E", year: 2008 },
+  { title: "American Beauty", year: 1999 },
+  { title: "The Dark Knight Rises", year: 2012 },
+  { title: "Princess Mononoke", year: 1997 },
+  { title: "Aliens", year: 1986 },
+  { title: "Oldboy", year: 2003 },
+  { title: "Once Upon a Time in America", year: 1984 },
+  { title: "Witness for the Prosecution", year: 1957 },
+  { title: "Das Boot", year: 1981 },
+  { title: "Citizen Kane", year: 1941 },
+  { title: "North by Northwest", year: 1959 },
+  { title: "Vertigo", year: 1958 },
+  {
+    title: "Star Wars: Episode VI - Return of the Jedi",
+    year: 1983,
+  },
+  { title: "Reservoir Dogs", year: 1992 },
+  { title: "Braveheart", year: 1995 },
+  { title: "M", year: 1931 },
+  { title: "Requiem for a Dream", year: 2000 },
+  { title: "Amélie", year: 2001 },
+  { title: "A Clockwork Orange", year: 1971 },
+  { title: "Like Stars on Earth", year: 2007 },
+  { title: "Taxi Driver", year: 1976 },
+  { title: "Lawrence of Arabia", year: 1962 },
+  { title: "Double Indemnity", year: 1944 },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    year: 2004,
+  },
+  { title: "Amadeus", year: 1984 },
+  { title: "To Kill a Mockingbird", year: 1962 },
+  { title: "Toy Story 3", year: 2010 },
+  { title: "Logan", year: 2017 },
+  { title: "Full Metal Jacket", year: 1987 },
+  { title: "Dangal", year: 2016 },
+  { title: "The Sting", year: 1973 },
+  { title: "2001: A Space Odyssey", year: 1968 },
+  { title: "Singin' in the Rain", year: 1952 },
+  { title: "Toy Story", year: 1995 },
+  { title: "Bicycle Thieves", year: 1948 },
+  { title: "The Kid", year: 1921 },
+  { title: "Inglourious Basterds", year: 2009 },
+  { title: "Snatch", year: 2000 },
+  { title: "3 Idiots", year: 2009 },
+  { title: "Monty Python and the Holy Grail", year: 1975 },
 ];
