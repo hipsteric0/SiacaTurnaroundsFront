cde810a27f6b4621db646fb6c493fe742d168248
mari1811 <maririos181@gmail.com>
2023-10-13 11:16:36 -0400

    Métricas prueba

--- siaca-turnarounds-front/src/pages/Metrics/index.tsx
+++ siaca-turnarounds-front/src/pages/Metrics/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./index.style.module.css";
 import { useState } from "react";
 import SiacaNavbar from "@/components/Reusables/Navbar/SiacaNavbar";
+import MetricsPage from "@/components/Metrics/MetricsMainPage";
 export default function Metrics() {
   //if token exists show regular html else show not signed in screen
   const [step, setStep] = useState(0);
@@ -8,7 +9,9 @@ export default function Metrics() {
   return (
     <main className={styles.mainContainerMachinesPage}>
       <SiacaNavbar activeMetricsValue={true} />
-      Metrics
+      <MetricsPage setStep={function (value: number): void {
+        throw new Error("Function not implemented.");
+      } } />
     </main>
   );
 }
